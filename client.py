0b3e1be54cddeb03cf40ec517556a750014d90c3
Zya Gurau <92696912+Zya-Gurau@users.noreply.github.com>
2024-01-21 11:41:39 +1300

    feat - Added initial files

---
+++ client.py
@@ -0,0 +1,320 @@
+"""Defines the client side for a socket based networking application
+
+Allows the client to send 'create' and 'read' requests to the server. 
+A 'create request allows the client to address a message for another client, 
+which is then stored in the server, a 'read' request gets the server to 
+send the Client all the messages addressed to them.
+
+Author: Zya Gurau
+Student Number: 64646853
+"""
+
+from socket import *
+import sys
+try:
+    # gets values from the arguments on the command line and preforms validity checks on them
+
+    if len(sys.argv) != 5:
+         raise ValueError("Request must include exactly four parameters")
+    
+    filename = sys.argv[0] # "client.py"
+
+    port = int(sys.argv[2]) # 5000
+
+    if port < 1024 or port > 64000:
+                raise ValueError("Port must be between 1024 and 64000 inclusive")
+    
+    name = sys.argv[3] # "zya gurau"
+
+    if len(name) < 1 or len(name.encode("utf-8")) > 255:
+         raise ValueError("user name must be at least one character and less than 255 bytes")
+
+    type_rw = sys.argv[4] # "read"
+    
+    if type_rw != 'read' and type_rw != 'create':
+         raise ValueError("request muse be of type 'read' or 'create'")
+    
+    services = getaddrinfo(sys.argv[1], port, AF_INET, SOCK_STREAM)
+    family, type, proto, canonname, address = services[0]
+
+except gaierror:
+    print("ERROR - '{argv[1]}' does not exist")
+    exit()
+except ValueError as err:
+    print("ERROR -  " + str(err))
+    exit()
+
+
+def get_response(s):
+    """Gets a message response from the server
+
+    Recieves data from the server in multiple parts, first recieves the header,
+    and chacks validity, then the length of sender name and length of messages, finally
+    recieves the senders name and message.
+    If there is a gap while reading parts of data error handling occurs.
+    
+    Args:
+        s (socket): the main client socket
+
+    Returns:
+        (None)
+    """
+
+    try:
+        # sets a timeout of one second on the socket throws a timeout error 
+        # if there is a gap in data
+        s.settimeout(1)
+        # recieves the first five bytes of the packet 
+        mes_res = s.recv(5)
+        req_array = bytearray(mes_res)
+        
+        # initialises variables from the recieved data
+        magic_no = req_array[0]<<8 | req_array[1]
+        r_id = req_array[2]
+        numitems = req_array[3]
+        moremsgs = req_array[4]
+        
+        # checks if header is valid
+        if magic_no != 0xAE73:
+            raise ValueError("magic number incorrect")
+        if r_id != 3:
+            raise ValueError("ID is not 3")
+        if moremsgs not in [0,1]:
+            raise ValueError("errouneous packet")
+        
+        # if there are no messages prints info and exit 
+        if numitems == 0:
+            print("no messages")
+            s.close()
+            exit()
+        
+        for i in range(numitems):
+            # recieves the next three bytes from the response
+            mes_res = s.recv(3)
+            req_array = bytearray(mes_res)
+
+
+            sender_len = req_array[0]
+            message_len = req_array[1]<<8 | req_array[2]
+
+            # checks the validity of the recieved data
+            if sender_len < 1:
+                raise ValueError("Length of sender name must be at least 1 - Erroneous packet")
+            if message_len < 1:
+                raise ValueError("Length of sender name must be at least 1 - Erroneous packet")
+
+            # recieves the senders name and message date
+            mes_res = s.recv(sender_len + message_len)
+            req_array = bytearray(mes_res)
+
+            # gets the senders name by iterating through the bytearray based on the length
+            # of the senders name
+            sender = []
+            for i in range(sender_len):
+                sender.append(req_array[i])
+                
+            # gets the message by iterating through the bytearray based on the length
+            # of the senders name and message starting from the end of the senders name
+            message = []
+            for i in range(sender_len,sender_len+message_len):
+                message.append(req_array[i])
+            
+            # decodes the senders name using utf-8
+            decode_sender_one = bytearray(sender)
+            decode_sender_two = bytes(decode_sender_one)
+            sender_done = decode_sender_two.decode("utf-8")
+            
+            # decodes the message using utf-8
+            decode_message_one = bytearray(message)
+            decode_message_two = bytes(decode_message_one)
+            message_done = decode_message_two.decode("utf-8")  
+            
+            print("Sender Name:")
+            print(sender_done)
+            print("")
+            print("Message:")
+            print(message_done)
+            print("")
+            print("")
+        
+        if moremsgs == 1:
+            print("more messages available from server")
+            print("")
+        s.close()
+        return None
+
+    except ValueError as err:
+        print("ERROR - " + str(err))
+        s.close()
+        exit()
+    except OSError as err:
+        print("ERROR - " + str(err))
+        s.close()
+        exit()
+    except UnicodeDecodeError:
+        print("ERROR - could not decode")
+        s.close()
+        exit()
+    except TimeoutError:
+        print("ERROR - Server timed out")
+        s.close()
+        exit()
+    except IndexError as err:
+        print("ERROR - " + str(err))
+        s.close()
+        exit()
+
+def get_input(s):
+    """Gets a clients input for a create request
+
+    Uses While true loops to get input and perform validity checks.
+    
+    Args:
+        s (socket): the main client socket
+    
+    Returns:
+        rec_name (str): the name of the reciever
+        message (str): the message written by the client
+    """
+
+    try:
+        while True:
+            rec_name = input("Enter Receiver Name: ")
+            if len(rec_name) < 1 or len(rec_name.encode("utf-8")) >= 255:
+                print("Reciever name must be at least 1 character long and must be less than 255 bytes")
+                continue
+            else:
+                break
+            
+        while True:
+            message = input("Enter Message: ")
+            if len(message) < 1 or len(message.encode("utf-8")) >= 65535:
+                print("message must be at least 1 character long and must be less than 65,535 bytes")
+                continue
+            else:
+                break 
+        return rec_name, message
+    except UnicodeEncodeError:
+        print("ERROR - could not encode")
+        s.close()
+        exit()
+
+def create_request_main(s):
+    """puts together a create request and sends it to the server
+    
+    Encodes data and appends it to a byte array which is then sent to the server,
+    exceptions are raised if errors occur in sending and encoding.
+
+    Args:
+        s (socket): the main client socket
+
+    Returns
+        (None)
+    """
+
+    try:
+        rec_name, message = get_input(s)
+        rec_name_bytes = rec_name.encode("utf-8")
+        message_bytes = message.encode("utf-8")
+
+        name_bytes = name.encode("utf-8")
+    except UnicodeEncodeError:
+        print("ERROR - could not encode")
+        s.close()
+        exit()
+        
+    message_request = bytearray()
+    message_request.append(0xAE)
+    message_request.append(0x73)
+
+    message_request.append(0x2)
+    message_request.append(len(name_bytes))
+    message_request.append(len(rec_name_bytes))
+    message_request.append(len(message_bytes)>>8)
+    message_request.append(0xff & len(message_bytes)) 
+
+    for byte in name_bytes:
+        message_request.append(byte)
+
+    for byte in rec_name_bytes:
+        message_request.append(byte)
+        
+    for byte in message_bytes:
+        message_request.append(byte)
+
+    try:
+        s.settimeout(1)
+        s.connect(address)  
+        s.send(message_request)
+        return None
+    except TimeoutError:
+        print("ERROR - Server timed out")
+        s.close()
+        exit()
+    except OSError as err:
+        print("ERROR -  " + str(err))
+        s.close()
+        exit()
+
+def read_request_main(s):
+    """Puts together a read request and sends it to the server 
+    
+    Args:
+        s (socket): the main client socket
+
+    Returns:
+        (None)
+    """
+
+    # Name is initialised globally 
+    name_bytes = name.encode("utf-8")
+
+
+    message_request = bytearray()
+    message_request.append(0xAE)
+    message_request.append(0x73)
+
+    message_request.append(0x1)
+    message_request.append(len(name_bytes))
+    message_request.append(0x0)    
+    message_request.append(0x0)
+    message_request.append(0x0)  
+
+    for byte in name_bytes:
+        message_request.append(byte)
+    
+    
+    try:
+        #connects to server and sends request
+        s.settimeout(1)
+        s.connect(address)  
+        s.send(message_request)
+        return None
+    except TimeoutError:
+        print("ERROR - Server timed out")
+        s.close()
+        exit()
+    except OSError as err:
+        print("ERROR -  " + str(err))
+        s.close()
+        exit()
+
+def main():
+    """Sets up read and create requests from the Client"""
+
+    #creates the main socket
+    s = socket(AF_INET, SOCK_STREAM)
+    # handles read request
+    if type_rw == 'read':
+        read_request_main(s)
+        get_response(s)
+    # handles create request
+    elif type_rw == 'create':
+        create_request_main(s)
+    s.close()
+
+
+main()
+    
+    
+    
---
+++ server.py
@@ -0,0 +1,346 @@
+"""Defines the server side for a socket based networking application
+
+allows clients to make 'read' or 'create' requests using bytearrays, using 'create' Clients can 
+send messages to the server addressed to other clients that the server will then store for them.
+Using a 'read' request a Client can get the server to send them up to 255 of the messages stored 
+for them by the server.
+
+Name: Zya Gurau
+Student Number: 64646853
+"""
+
+from socket import *   
+import sys
+
+#the dict used to store Client messages
+messages = dict()
+
+#gets the prt number from command lines arguments and check if its valid
+try:
+        port = int(sys.argv[1]) # 50000
+        if port < 1024 or port > 64000:
+                raise ValueError()
+except TypeError:
+        print("ERROR - Port must be a number")
+        exit()
+
+except ValueError:
+        print("ERROR - Port must be between 1024 and 64000 inclusive")
+        exit()
+
+
+def create_initial_response(message_response, num_items, more_msgs):
+        """returns the basic packet header for a read request
+        
+        Args: 
+                message_response (bytearray): The bytearray that will contain the message response
+                num_items (int): The number of messages addressed to the client
+                is_empty (bool): A flag indicating whether there are no messages addressed to the client
+        
+        Returns:
+                message_response (bytearray): The bytearray containing the packet header for the message response
+        """       
+                        
+        message_response.append(0xAE)
+        message_response.append(0x73) 
+        message_response.append(0x3)
+        message_response.append(num_items)
+        message_response.append(more_msgs)
+        
+        return message_response
+
+def add_messages(message_response, items, num_items, s, c):
+        """Adds messages to the message response bytearray
+
+        Iterates through the messages stored for the client in range of the number of items
+        able to be sent and adds them the the response 
+        
+        Args:
+                message_response (bytearray): The bytearray containing the message response header
+                items (list): The list of messages addressed to the client
+                num_items (int): The number of messages able to be added to the bytearray
+                s (socket): The server socket
+                c (socket): The connection socket
+
+        Returns:
+                message_response (bytearray): The bytearray containing the packet header and the 
+                        messages addressed to the client
+        """
+
+        try:
+
+                for i in range(num_items):
+                        message_bytes = items[i][1]
+                
+                        sender_bytes = items[i][0].encode("utf-8")
+                                        
+                        message_response.append(len(sender_bytes))
+                        message_response.append(len(message_bytes)>>8)
+                        message_response.append(0xff & len(message_bytes)) 
+                        
+                        for byte in sender_bytes:
+                                message_response.append(byte)
+                        for byte in message_bytes:
+                                message_response.append(byte)
+                return message_response
+        except UnicodeEncodeError:
+                print("ERROR - could not encode message")
+                s.close
+                c.close()
+                exit()
+
+def create_response_message(sen_name, s, c):
+        """Creates a message response to a 'read' request
+        
+        Args: 
+                sen_name (str): The name of the client who sent the 'read' request
+                s (socket): The server socket
+                c (socket): The connection socket
+        
+        Returns:
+                num_items (int): The number of messsages included in the message_response
+                message_response (bytearray): The bytearray containing the message response
+        """
+
+        num_items = 0
+        more_msgs = 0
+        message_response = bytearray()
+
+        # generates the packet header and adds the saved messages to the packet
+        if sen_name in messages.keys():
+                items = messages[sen_name]
+
+                # the number of messages stored for the Client
+                num_items = len(messages[sen_name]) 
+
+                # sets a message send limit of 255 
+                # If there are more than 255 messages for the client then 255 are sent with
+                # a flag set indicating there are more messages available
+                if num_items > 255:
+                        more_msgs = 1
+                        num_items = 255
+                elif num_items == 0:
+                        more_msgs = 0
+                        num_items = 0 
+
+                message_response = create_initial_response(message_response, num_items, more_msgs)    
+                message_response = add_messages(message_response, items, num_items, s, c)    
+
+        # generats a packet header indicating there no messages stored for the client           
+        else:
+                message_response = create_initial_response(message_response, num_items, more_msgs)  
+        return num_items, message_response
+
+def read_request(name_len, req_array, s, c):
+        """Gathers the necessary data to handle a clients 'read' request
+        
+        Args:
+                name_len (int): The number of bytes the clients name takes up in the message request bytearray
+                req_array (bytearray): The bytearray containing the clients 'read' request
+                s (socket): The server socket
+                c (socket): The connection socket
+
+        Returns:
+                sen_name (str): The name of the client
+                num_items (int): The number of messages included in the message response
+                message_response (bytearray): The bytearray containing the message response
+        """
+
+        sen_name = get_name(req_array, 7, 7+name_len, s, c)
+        num_items, message_response = create_response_message(sen_name, s, c)   
+        return sen_name, num_items, message_response
+
+def get_name(req_array, range_val_one, range_val_two, s, c):
+        """Get a name from a message request array
+
+        Uses a 'loop in range' to extract the relevant data to decode the name
+        
+        Args:
+                req_array (bytearray): The bytearray containing the clients 'read' request
+                range_val_one (int): The lower bound for the range loop
+                range_val_two (int): The upper bound for the range loop
+                s (socket): The server socket
+                c (socket): The connection socket
+
+        Returns:
+                name (str): The decoded name
+        """
+
+        try:
+                name_array = []
+                for i in range(range_val_one, range_val_two):
+                        name_array.append(req_array[i])
+                dec_name = bytearray(name_array)
+                byte_name = bytes(dec_name)
+                name = byte_name.decode("utf-8")
+                return name
+        except UnicodeDecodeError:
+                print("ERROR - could not decode")
+                c.close()
+                s.close()
+                exit()
+
+def get_message(req_array, range_val_one, range_val_two):
+        """Gets the message from a clients 'create' request
+        
+        Uses a 'loop in range' to extract the message data
+
+        Args:
+                req_array (bytearray): The bytearray containing the clients 'create' request
+                range_val_one (int): The lower bound for the range loop
+                range_val_two (int): The upper bound for the range loop
+
+        Returns:
+                message (bytearray): The bytearray containing the message data
+        """
+        message = []
+        for i in range(range_val_one, range_val_two):
+                message.append(req_array[i])
+        return bytearray(message)        
+        
+def create_request(req_array, name_len, receiver_len,s,c):
+        """Handles a clients 'create' request 
+        
+        Args:
+                req_array (bytearray): The bytearray containing the clients 'create' request
+                name_len (int): The number of bytes the clients name takes up in the message request bytearray
+                receiver_len (int): The number of bytes the recievers name take up in the message request bytearray
+                s (socket): The server socket
+                c (socket): The connection socket
+
+        Returns: 
+                send_name (str): The name of the client
+                rec_name (str): The name of the reciever
+        """
+        
+        rec_name = get_name(req_array, 7+name_len, 7+receiver_len+name_len,s,c)
+        send_name = get_name(req_array, 7, 7+name_len,s,c)
+          
+        dec_mes = get_message(req_array, name_len + receiver_len + 7, len(req_array))
+    
+        # stores the recieved message under the intended recievers name
+        if rec_name not in messages.keys():
+                # creates array if there are no messages stored for the receiver
+                messages[rec_name] = [(send_name,dec_mes)]
+        else:
+                # if there are already messages for the reciever then new message is added to the array
+                messages[rec_name].append((send_name,dec_mes))    
+        return send_name, rec_name
+ 
+def server_loop(s):
+        """listens and recieves message request froma client
+        
+        Decodes the message request header and handles 'read' and 'create' requests
+
+        Args:
+                s (socket): The server socket  
+        """
+
+        try:
+                # accepts an incoming connection request
+                c, addr = s.accept() 
+                # set the timeout length for the connection socket 
+                c.settimeout(1)
+                print ('Got connection from', addr )
+                
+                # recieves a message request from the connection socket
+                message_req = c.recv(66052)
+                req_array = bytearray(message_req)
+                
+                # uses bitwise operations on the bytearray to exract the header data
+                magic_no = req_array[0]<<8 | req_array[1]
+                r_id = req_array[2]
+                name_len = req_array[3]
+                receiver_len = req_array[4]
+                message_len = req_array[5]<<8 | req_array[6]
+                
+                # checks the validity of the recieved data
+                if magic_no != 0xAE73:
+                        raise ValueError("magic number incorrect")
+                if r_id != 1 and r_id != 2:
+                        raise ValueError("ID incorrect")
+                if name_len < 1:
+                        raise ValueError("Name length less than 1")
+                if (r_id == 1 and receiver_len != 0) or (r_id == 2 and receiver_len < 1):
+                        raise ValueError("reciever length incorrect")
+                if (r_id == 1 and message_len != 0) or (r_id == 2 and message_len < 1):
+                        raise ValueError("message length incorrect")  
+
+                # if it's a create request
+                if r_id == 2:
+                        send_name, rec_name = create_request(req_array, name_len, receiver_len,s,c)
+                        print(send_name + " has created a message for " + rec_name)
+                        c.close()
+                        return None
+                
+                # if it's a read request
+                if r_id == 1:
+                        sen_name, num_items, message_response = read_request(name_len, req_array, s, c)
+
+                        # if messages are sent info message is printed and the sent messages are removed form
+                        # the message dictionary
+                        if num_items > 0:
+                                print("sent " + str(num_items) + " messages to " + sen_name)
+                        
+                                # iterates though the messages stored under the receivers name and deletes each
+                                # message in the range of 0 - number of messages sent
+                                for i in range(num_items):
+                                        messages[sen_name].pop(0) 
+                        
+                        # if no messages are sent
+                        else:
+                                print("no messages sent")      
+                        # sends a message response via the connection socket
+                        c.send(message_response)
+                        # closes the connection socket
+                        c.close()     
+                        return None   
+        except OSError as err:
+                print("ERROR -  " + str(err))
+                c.close()
+                s.close()
+                exit()
+        except TimeoutError:
+                print("ERROR - timed out")
+                c.close()
+                s.close()
+                exit()
+        except ValueError as err:
+                print("ERROR -  " + str(err))
+                c.close()
+                s.close()
+                exit()
+
+#DELETE THIS BEFORE SUBMISSION
+def test():
+        test_array = bytearray()
+
+        pass
+
+def main():
+        try:
+                
+                # server socket is created
+                s = socket(AF_INET, SOCK_STREAM)     
+                # socket is bound to a given port and an ip address "0.0.0.0" is used 
+                # to bind to all local interfaces    
+                s.bind(('0.0.0.0', port))        
+                print ("socket binded to %s" %(port))
+
+                #Listen for connection requests
+                s.listen()    
+                print ("socket is listening")    
+                
+        except OSError as err:
+                print("ERROR -  " + str(err))
+                s.close()
+                exit()
+        while True:
+                server_loop(s)
+
+main()
+
+    
+  
+
+
